39abc36f804cf747f2f6b545a8b2395bc00c7ae5
Vũ Duy Bắc <duybacdno@gmail.com>
2026-05-08 10:12:04 +0700

    fix UI conversation

--- app/api/v1/messages.py
+++ app/api/v1/messages.py
@@ -68,8 +68,8 @@ def get_messages(
     messages = (
         db.query(Message)
         .options(joinedload(Message.employee))
-        .filter(Message.conversation_id == conversation_uuid),
-        Message.kind == MessageKind.COMMENT
+        .filter(Message.conversation_id == conversation_uuid)
+        .filter(Message.kind == MessageKind.COMMENT)
         .order_by(Message.created_at)
         .all()
     )
